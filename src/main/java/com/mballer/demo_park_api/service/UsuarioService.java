27ffe60ab4038399abff8750c521937188e8dd14
DeivisDigiHub <161029225+DeivisDigiHub@users.noreply.github.com>
2024-07-09 11:22:00 -0300

    iniciando o projeto

--- src/main/java/com/mballer/demo_park_api/service/UsuarioService.java
+++ src/main/java/com/mballer/demo_park_api/service/UsuarioService.java
@@ -13,7 +13,5 @@ public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
 
-    public UsuarioService(UsuarioRepository usuarioRepository) {
-        this.usuarioRepository = usuarioRepository;
-    }
+
 }
