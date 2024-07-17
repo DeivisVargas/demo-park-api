260b3e8633be33abba9137ad20c57435acec8a8b
DeivisDigiHub <161029225+DeivisDigiHub@users.noreply.github.com>
2024-07-17 17:12:00 -0300

    configurando dto de alteração de senha

--- src/main/java/com/mballer/demo_park_api/service/UsuarioService.java
+++ src/main/java/com/mballer/demo_park_api/service/UsuarioService.java
@@ -18,6 +18,7 @@ public class UsuarioService {
 
     @Transactional
     public Usuario salvar(Usuario usuario) {
+
         return usuarioRepository.save(usuario);
     }
 
@@ -34,10 +35,21 @@ public class UsuarioService {
     }
 
 
+    //edita senha após os testes
     @Transactional
-    public Usuario editarSenha(Long id , String password){
+    public Usuario editarSenha(Long id , String senhaAtual, String novaSenha, String confirmaSenha){
+        if(!novaSenha.equals(confirmaSenha)){
+            throw new RuntimeException("Nova senha não confere com a confirmação de senha");
+        }
+
         Usuario user = buscarPorId(id) ;
-        user.setPassword(password);
+
+        //verifica senha antiga
+        if(!user.getPassword().equals(senhaAtual)){
+            throw new RuntimeException("Sua senha não confere!");
+        }
+
+        user.setPassword(novaSenha);
         return  user;
     }
 }
--- src/main/java/com/mballer/demo_park_api/web/controller/UsuarioController.java
+++ src/main/java/com/mballer/demo_park_api/web/controller/UsuarioController.java
@@ -5,6 +5,7 @@ import com.mballer.demo_park_api.entity.Usuario;
 import com.mballer.demo_park_api.service.UsuarioService;
 import com.mballer.demo_park_api.web.dto.UsuarioCreateDto;
 import com.mballer.demo_park_api.web.dto.UsuarioResponseDto;
+import com.mballer.demo_park_api.web.dto.UsuarioSenhaDto;
 import com.mballer.demo_park_api.web.dto.mapper.UsuarioMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -30,17 +31,17 @@ public class UsuarioController
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Usuario> getById(@PathVariable Long id){
+    public ResponseEntity<UsuarioResponseDto> getById(@PathVariable Long id){
         Usuario user =  usuarioService.buscarPorId(id);
-        return  ResponseEntity.ok(user);
+        return  ResponseEntity.ok(UsuarioMapper.toDto(user));
     }
 
 
-
+    //alteração de senha
     @PatchMapping("/{id}")
-    public ResponseEntity<Usuario> updatePassword(@PathVariable Long id, @RequestBody Usuario usuario){
-        Usuario user =  usuarioService.editarSenha(id , usuario.getPassword());
-        return  ResponseEntity.ok(user);
+    public ResponseEntity<Void> updatePassword(@PathVariable Long id, @RequestBody UsuarioSenhaDto dto){
+        Usuario user =  usuarioService.editarSenha(id , dto.getSenhaAtual(), dto.getNovaSenha() , dto.getConfirmaSenha());
+        return  ResponseEntity.noContent().build();
     }
 
     @GetMapping()
